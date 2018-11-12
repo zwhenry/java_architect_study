469e0ebe12650d5c50b222b2f52b6a78c083abb6
zhangwei10 <zhangwei10@dankegongyu.com>
2018-11-12 12:21:58 +0800

    netty 增加注释和文档

--- zhangwei-netty-study/src/main/java/com/zhangwei/netty/netty/ServiceHandler.java
+++ zhangwei-netty-study/src/main/java/com/zhangwei/netty/netty/ServiceHandler.java
@@ -4,18 +4,38 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.InetAddress;
+import java.util.Date;
+
 /**
- * ${DESCRIPTION}
+ * 服务相关的设置的代码写完之后，我们再来编写主要的业务代码。
+ * 使⽤用Netty编写 [业务层 ]的代码， 我们需要继承 ChannelInboundHandlerAdapter 或 SimpleChannelInboundHandler 类，在这 ⾥里里顺便便说下它们两的区别吧。
+ * 继承 SimpleChannelInboundHandler 类之后，会在接收到数据后会 ⾃自动 release 掉数据占⽤用的 Bytebuffer 资源。并且继承该类需要指定数据格式。
+ * 而继 承ChannelInboundHandlerAdapter 则不不会⾃自动释放，需要⼿手动调⽤用 ReferenceCountUtil.release() 等⽅方法进⾏行行释放。继承该类不不需要指定数据格式。
+ * 所以在这⾥里里，个⼈人 推荐服务端继承 ChannelInboundHandlerAdapter ，⼿手动进⾏行行释放，防⽌止数据未处理理完就⾃自动释放 了了。
+ * ⽽而且服务端可能有多个客户端进⾏行行连接，并且每⼀一个客户端请求的数据格式都不不⼀一致，这时便便可 以进⾏行行相应的处理理。
+ * 客户端根据情况可以继承 SimpleChannelInboundHandler 类。好处是直接指 定好传输的数据格式，就不不需要再进⾏行行格式的转换了了。
  *
  * @author zhangwei10
  * @create 2018-11-10 12:30 PM
  * @since 1.0.0
  **/
 @Slf4j
+//@Sharable  这⾥里里有个注解， 该注解 Sharable 主要是为了了多个handler可以被多个channel安全地共享，也就是保 证线程安全。
 public class ServiceHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 建⽴立连接时，发送⼀一条庆祝消息
+     *
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         log.info("chanelActive>>>>>>>");
+        // 为新连接发送庆祝
+        ctx.write("Welcome to " + InetAddress.getLocalHost().getHostName() + "!/r/n");
+        ctx.write("It is " + new Date() + " now./r/n");
+        ctx.flush();
     }
 
     @Override
@@ -25,6 +45,12 @@ public class ServiceHandler extends ChannelInboundHandlerAdapter {
         ctx.close();
     }
 
+    /**
+     * 异常处理理
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         log.info("get server exception :" + cause.getMessage());
--- zhangwei-netty-study/src/main/java/com/zhangwei/netty/netty/ServiceNetty.java
+++ zhangwei-netty-study/src/main/java/com/zhangwei/netty/netty/ServiceNetty.java
@@ -4,9 +4,13 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,34 +31,52 @@ public class ServiceNetty {
 
     private static final int BIZTHREADSIZE = 100;
 
+    //Configure the server
+    //创建两个EventLoopGroup对象
+    //创建boss线程组 ⽤用于服务端接受客户端的连接
+    //    boss 线程组: ⽤用于服务端接受客户端的 连接 。连接
+    //    worker 线程组: ⽤用于进⾏行行客户端的SocketChannel的 数据读写 。
     private static final EventLoopGroup bossGroup = new NioEventLoopGroup(BIZGROUPSIZE);
+    // 创建 worker 线程组 ⽤用于进⾏行行 SocketChannel 的数据读写
     private static final EventLoopGroup workGroup = new NioEventLoopGroup(BIZTHREADSIZE);
 
 
     public static void start() throws InterruptedException {
+        // 创建 ServerBootstrap 对象
         ServerBootstrap serverBootstrap = new ServerBootstrap();
+        //设置使⽤用的EventLoopGroup
         serverBootstrap.group(bossGroup, workGroup)
+                //设置要被实例例化的为 NioServerSocketChannel 类
                 .channel(NioServerSocketChannel.class)
+                // 设置 NioServerSocketChannel 的处理理器器
+                .handler(new LoggingHandler(LogLevel.INFO))
+                // 设置连⼊入服务端的 Client 的 SocketChannel 的处理理器器
                 .childHandler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) throws Exception {
                         ChannelPipeline pipeline = ch.pipeline();
+                        // 添加帧限定符来防⽌止粘包现象
+                        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
                         pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+                        // 解码和编码，应和客户端⼀一致
                         pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
                         pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+                        // 业务逻辑实现类
                         pipeline.addLast(new ServiceHandler());
                     }
                 });
 
-
+        // 绑定端⼝口，并同步等待成功，即启动服务端 sync好像不需要
+        //先 调⽤用 #bind(int port) ⽅方法，绑定端⼝口， 后 调⽤用 ChannelFuture#sync() ⽅方 法，阻塞等待成功。这个过程，就是“ 启动服务端 ”。
         ChannelFuture channelFuture = serverBootstrap.bind(IP, PORT).sync();
-
+        // 监听服务端关闭，并阻塞等待
         channelFuture.channel().closeFuture().sync();
         log.info("server start");
     }
 
 
     protected static void shutdown() {
+        //关闭两个 EventLoopGroup 对象
         workGroup.shutdownGracefully();
         bossGroup.shutdownGracefully();
     }
